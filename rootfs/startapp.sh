8f36943db038850c21d7f589c33df8fc4e7c762d
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-08-30 22:11:35 -0400

    Added variable to process or clear the HandBrake GUI queue.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,6 +6,11 @@ export LIBGL_ALWAYS_SOFTWARE=true
 
 COMMON_ARGS="--config /config"
 
+case "$(echo "${HANDBRAKE_GUI_QUEUE_STARTUP_ACTION:-NONE}" | tr '[:upper:]' '[:lower:]')" in
+    process) COMMON_ARGS="$COMMON_ARGS --auto-start-queue" ;;
+    clear) COMMON_ARGS="$COMMON_ARGS --clear-queue" ;;
+esac
+
 cd /storage
 if [ "${HANDBRAKE_DEBUG:-0}" -eq 1 ]; then
   exec /usr/bin/ghb $COMMON_ARGS --debug >> /config/log/hb/handbrake.debug.log
