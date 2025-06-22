4d4daddd608a0df44e5679cfdc16c3405135aefc
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-21 21:34:56 -0400

    Revert "Set the GDK_GL variable to prevent error message in log."
    This reverts commit 89bcb9aadc5b671db3139c480018450a09be3ab9.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -4,14 +4,6 @@ export HOME=/config
 export GTK_A11Y=none
 export LIBGL_ALWAYS_SOFTWARE=true
 
-# Added to avoid the following error message:
-#   MESA-LOADER: failed to open swrast: Error loading shared library
-#   /usr/lib/xorg/modules/dri/swrast_dri.so: No such file or directory
-#   (search paths /usr/lib/xorg/modules/dri, suffix _dri)
-# We could instead install `mesa-dri-gallium`, but this increases the image
-# size a lot.
-export GDK_GL=disable
-
 COMMON_ARGS="--config /config"
 
 case "$(echo "${HANDBRAKE_GUI_QUEUE_STARTUP_ACTION:-NONE}" | tr '[:upper:]' '[:lower:]')" in
