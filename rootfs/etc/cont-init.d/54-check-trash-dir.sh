bd60712a1c113e28f6431c33702969923edb994c
Jocelyn Le Sage <jocelyn@le-sage.com>
2025-06-22 11:02:05 -0400

    Added variable to customize the trash directory. The trash feature can now be customized for each watch folder.

--- rootfs/etc/cont-init.d/54-check-trash-dir.sh
+++ rootfs/etc/cont-init.d/54-check-trash-dir.sh
@@ -7,22 +7,36 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 if is-bool-val-true "${AUTOMATED_CONVERSION_USE_TRASH:-0}"; then
-    if [ -f /trash/.not_mapped ]; then
-        echo "ERROR: Trash usage is enabled, but trash directory not mapped to the host."
-        exit 1
-    fi
+    for i in $(seq 1 ${AUTOMATED_CONVERSION_MAX_WATCH_FOLDERS:-5}); do
+        if [ "$i" -eq 1 ]; then
+            TRASH_DIR="${AUTOMATED_CONVERSION_TRASH_DIR:-/trash}"
+        else
+            eval "TRASH_DIR=\"\${AUTOMATED_CONVERSION_TRASH_DIR_${i}:-}\""
+        fi
+        [ -n "$TRASH_DIR" ] || continue
 
-    set +e
-    TMPFILE="$(su-exec "$USER_ID:$GROUP_ID" mktemp /trash/.test_XXXXXX)"
-    RC=$?
-    set -e
-    if [ $RC -eq 0 ]; then
-        # Success, we were able to write a file.
-        su-exec "$USER_ID:$GROUP_ID" rm "$TMPFILE"
-    else
-        echo "ERROR: Trash usage is enabled, but no write permission on it."
-        exit 1
-    fi
+        if [ -f "$TRASH_DIR"/.not_mapped ]; then
+            echo "ERROR: Trash usage is enabled, but trash directory '$TRASH_DIR' is not mapped to the host."
+            exit 1
+        fi
+
+        if [ ! -d "$TRASH_DIR" ]; then
+            echo "ERROR: Trash usage is enabled, but trash directory '$TRASH_DIR' is not found."
+            exit 1
+        fi
+
+        set +e
+        TMPFILE="$(su-exec "$USER_ID:$GROUP_ID" mktemp "$TRASH_DIR"/.test_XXXXXX)"
+        RC=$?
+        set -e
+        if [ $RC -eq 0 ]; then
+            # Success, we were able to write a file.
+            su-exec "$USER_ID:$GROUP_ID" rm "$TMPFILE"
+        else
+            echo "ERROR: Trash usage is enabled, but trash directory '$TRASH_DIR' is not writable."
+            exit 1
+        fi
+    done
 fi
 
 # vim:ts=4:sw=4:et:sts=4
